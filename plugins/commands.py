8842d0bf539e41e426174f9caffda385088e1bf6
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-03-04 02:53:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -57,10 +57,10 @@ async def start(client, message):
                     InlineKeyboardButton('🔍 Tᴏᴘ Sᴇᴀʀᴄʜɪɴɢ', callback_data="topsearch"),
                     InlineKeyboardButton(' DVDRIPS 📀', url=f'https://t.me/EarlywatchBot')
                 ],[
+                    InlineKeyboardButton('Dᴏɴᴀᴛɪᴏɴ 🌺', callback_data='donation'),
                     InlineKeyboardButton('🦋 Pʀᴇᴍɪᴜᴍ', callback_data='seeplans'),
                     InlineKeyboardButton('Rᴇғᴇʀ 🌺', callback_data="reffff")
                 ],[
-                   # InlineKeyboardButton('Dᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'),
                     InlineKeyboardButton('🪅 Mᴀɪɴ Cʜᴀɴɴᴇʟ 🪅', url=f'https://t.me/Telugu_Movies_999')
                 ],[
                     InlineKeyboardButton('🔖 ᴍɪᴅᴅʟᴇ ᴄʟᴀss ʙᴇꜱᴛ ᴅᴇᴀʟꜱ 🔖', url=f'https://t.me/Vs_Best_Deals')
@@ -94,10 +94,10 @@ async def start(client, message):
                     InlineKeyboardButton('🔍 Tᴏᴘ Sᴇᴀʀᴄʜɪɴɢ', callback_data="topsearch"),
                     InlineKeyboardButton(' DVDRIPS 📀', url=f'https://t.me/EarlywatchBot')
                 ],[
+                    InlineKeyboardButton('Dᴏɴᴀᴛɪᴏɴ 🌺', callback_data='donation'),
                     InlineKeyboardButton('🦋 Pʀᴇᴍɪᴜᴍ', callback_data='seeplans'),
                     InlineKeyboardButton('Rᴇғᴇʀ 🌺', callback_data="reffff")
                 ],[
-                   # InlineKeyboardButton('Dᴏɴᴀᴛɪᴏɴ 💰', callback_data='donation'),
                     InlineKeyboardButton('🪅 Mᴀɪɴ Cʜᴀɴɴᴇʟ 🪅', url=f'https://t.me/Telugu_Movies_999')
                 ],[
                     InlineKeyboardButton('🔖 ᴍɪᴅᴅʟᴇ ᴄʟᴀss ʙᴇꜱᴛ ᴅᴇᴀʟꜱ 🔖', url=f'https://t.me/Vs_Best_Deals')
