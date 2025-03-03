0d857379c8cb56893c6faa1e86fee18b35fe4218
NewAryabro <138558338+NewAryabro@users.noreply.github.com>
2025-03-04 00:11:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -76,7 +76,7 @@ CAPTION_LANGUAGES = ["Bhojpuri", "Hindi", "Bengali", "Tamil", "English", "Bangla
 # ============================
 # Verification Settings
 # ============================
-VERIFY = bool(environ.get('VERIFY',False)  # Verification On (True) / Off (False)
+VERIFY = bool(environ.get('VERIFY',False))  # Verification On (True) / Off (False)
 DEENDAYAL_VERIFY_EXPIRE = int(environ.get('DEENDAYAL_VERIFY_EXPIRE', 6))  # Add time in hours
 DEENDAYAL_VERIFIED_LOG = int(environ.get('DEENDAYAL_VERIFIED_LOG', '-1002370043591'))  # Log channel id (make sure bot is admin)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', 'https://t.me/vs_Files_Mx_player/230')  # How to open tutorial link for verification
